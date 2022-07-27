89d7a4d4e470d7d1db14d0cd7b48db4a1bd3a1de
abdulawalemon1 <emondx4@gmail.com>
2022-07-28 02:41:26 +0600

    form validation added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,35 +1,53 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 
 const Form = () => {
+
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        console.log(data)
+    };
+
     return (
         <div>
             <div class="hero min-h-screen bg-base-200">
                 <div class="hero-content">
 
                     <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <div class="card-body">
+                        <form onSubmit={handleSubmit(onSubmit)} class="card-body">
                             <h1>Receipt Details</h1>
                             <div class="form-control">
                                 <label class="label">
                                     <span class="label-text">Date <span className='text-red-500'>*</span></span>
                                 </label>
-                                <input type="date" placeholder="Enter Date" class="input input-bordered" />
+                                <input {...register("date", { required: true })} type="date" placeholder="Enter Date" class="input input-bordered" />
+                                <input />
+                                {errors.date?.type === 'required' && <p className='text-red-500 text-xs'>Date is required</p>}
                             </div>
                             <div class="form-control">
                                 <label class="label">
                                     <span class="label-text">Amount <span className='text-red-500'>*</span></span>
                                 </label>
-                                <input type="number" placeholder="Enter Amount (in INR)" class="input input-bordered" />
+                                <input {...register("amount",
+                                    {
+                                        required: { value: true, message: 'Amount is required!' },
+                                        min: 1,
+
+                                    })}
+                                    type="number" placeholder="Enter Amount (in INR)" class="input input-bordered" />
+                                {errors.amount?.type === 'required' && <p className='text-red-500 text-xs'>Amount is required</p>}
+                                {errors.amount?.type === 'min' && <p className='text-red-500 text-xs'>Enter a valid amount!</p>}
                             </div>
                             <div class="form-control">
                                 <label class="label">
                                     <span class="label-text">Payment Mode<span className='text-red-500'>*</span></span>
                                 </label>
-                                <select class="select select-bordered w-full max-w-xs">
-                                    <option disabled selected>Select</option>
-                                    <option>Cash</option>
-                                    <option>Card</option>
+                                <select {...register("paymode", { required: true })} class="select select-bordered w-full max-w-xs">
+                                    <option value="cash">cash</option>
+                                    <option value="card">card</option>
                                 </select>
+                                {errors.paymode?.type === 'required' && <p className='text-red-500 text-xs'>Select payment method!</p>}
 
                             </div>
                             <div class="form-control">
@@ -43,10 +61,10 @@ const Form = () => {
                                     <button class="btn btn-error">Cancel</button>
                                 </div>
                                 <div class="form-control mt-6">
-                                    <button class="btn btn-success">Submit</button>
+                                    <input type='submit' class="btn btn-success" />
                                 </div>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
